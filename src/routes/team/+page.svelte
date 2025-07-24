7b5141ebc19bd907691fc05940d7a5b2e07e7466
Thomas Jansen <thomas.jansen@t1-software.com>
2025-07-24 21:40:52 +0200

    fix

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -63,7 +63,7 @@ kennen.
 
     <div class="text-center text-sm flex flex-col gap-1 align-center">
         <div class="flex justify-center"
-            ><div class="w-80 h-80 bg-gradient-to-tr from-gulfstream-300 to-gulfstream-400" /></div
+            ><img src="/heinz_julia.jpg" alt="Julia Heinz" class="w-80 h-80 object-cover" /></div
         >
         <div class="font-semibold">Julia Heinz</div>
         <div>Medizinische Fachangestellte</div>
