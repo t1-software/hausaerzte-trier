c5c0f836df8436952acbc38ca723a7d42bda4db8
Thomas Jansen <thomas.jansen@t1-software.com>
2025-11-21 21:15:10 +0100

    variable top image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,32 @@
     import type { LayoutData } from "./$types";
     import Footer from "../components/Footer.svelte";
     import Header from "../components/Header.svelte";
+    import { page } from "$app/stores";
 
     export let data: LayoutData;
 
     $: content = data.content;
     $: important = content["Wichtig"]?.[0]?.[0]?.replaceAll("\r", "") || "";
+
+    // Determine header image based on current route
+    $: headerImage = getHeaderImage($page.route.id);
+
+    function getHeaderImage(routeId: string | null): string {
+        if (routeId === "/team") {
+            return "praxis3.jpeg";
+        } else if (routeId === "/leistungsspektrum") {
+            return "praxis1.jpeg";
+        }
+        return "praxis2.jpeg"; // default image
+    }
 </script>
 
 <div class="flex flex-col min-h-screen">
     <div class="flex flex-col">
         <Header {content} />
         <div
-            class="absolute -z-10 bg-[url('/praxis2.jpeg')] bg-cover bg-center bg-no-repeat opacity-80 w-full lg:h-[500px] md:h-[400px] h-[300px]"
+            class="absolute -z-10 bg-cover bg-center bg-no-repeat opacity-80 w-full lg:h-[500px] md:h-[400px] h-[300px]"
+            style="background-image: url('/{headerImage}')"
         >
             <div class="w-full h-full max-w-7xl relative mx-auto">
                 <div
